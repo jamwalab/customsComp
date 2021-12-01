4467aba3450cad3f1eba6fb1c852f1cf835d3e89
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-11-30 23:07:56 -0500

    starter files

---
+++ server/server.js
@@ -0,0 +1,23 @@
+//-----DECLATATIONS-----//
+const express = require('express');
+const sequelize = require('./config/connection');
+//const routes = require('./controllers');
+//const path = require('path');
+const cors = require('cors')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//-----MIDDLEWARE-----//
+app.use(cors())
+//Parse incoming string or array
+app.use(express.urlencoded({extended: true}));
+//Parse incoming Json data
+app.use(express.json());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => console.log('NOW LISTENING'));
+});
